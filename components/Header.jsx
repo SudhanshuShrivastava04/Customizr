0151f0789c28393af1c8c4358a1ec7c690a01a3a
Sudhanshu Shrivastava <211230053@nitdelhi.ac.in>
2024-07-31 01:32:12 +0530

    Project Setup

---
+++ components/Header.jsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import { Button } from "./ui/button";
+import { IoIosArrowRoundForward } from "react-icons/io";
+
+const Header = () => {
+  return (
+    <div className="sticky top-0 md:p-3 p-2 border-b shadow-sm glassmorphism z-10">
+      <div className="flex items-center justify-between">
+        <Image src={"/logo.svg"} width={50} height={50} alt="logo" />
+        <Button className="flex gap-x-2">
+          Get Started
+          <IoIosArrowRoundForward size={24} />
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
