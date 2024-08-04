71e4ad7f6df3654c9ce1bd99a6fbb6173c1ea153
Sudhanshu Shrivastava <211230053@nitdelhi.ac.in>
2024-08-04 19:12:52 +0530

    Frontend : No forms ui and create form modal

---
+++ components/AddPromptModal.jsx
@@ -0,0 +1,36 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+import React from "react";
+import { Button } from "./ui/button";
+import { Plus } from "lucide-react";
+
+const AddPromptModal = () => {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button className="flex gap-x-2">
+          Create Form
+          <Plus size={18} />
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default AddPromptModal;
